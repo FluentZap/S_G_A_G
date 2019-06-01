1454fc429c0ccd25ca5c242cd5cf8390b1110e73
Todd Aden <FluentZap@Gmail.com>
2019-05-31 17:02:39 -0700

    add life expectancy tests

--- test/ageGenerator.test.js
+++ test/ageGenerator.test.js
@@ -43,3 +43,50 @@ describe('Galactic Age', () => {
   
 
 });
+
+//31536000000
+describe('Galactic Life Expectancy', () => {
+  let age;
+  beforeEach(() => {
+    age = new GalacticAge("March 15 1952 UTC");
+  });
+
+  test('gets life expectancy on Earth', () => {
+    expect(age.getGalacticLongevity("Earth")).toEqual(1);
+  });
+
+  test('gets life expectancy on Mercury', () => {
+    expect(age.getGalacticLongevity("Mercury")).toEqual(0);
+  });
+
+  test('gets life expectancy on Venus', () => {
+    expect(age.getGalacticLongevity("Venus")).toEqual(0);
+  });
+
+  test('gets life expectancy on Mars', () => {
+    expect(age.getGalacticLongevity("Mars")).toEqual(1);
+  });
+
+  test('gets life expectancy on Jupiter', () => {
+    expect(age.getGalacticLongevity("Jupiter")).toEqual(11);
+  });
+
+
+  test('converts 25 year old with birthday to 47 Mars years', () => {
+    //build a mock function and overwrite the default js function
+    //mock function will always return 1593561600000 July 1st 2020 UTC
+    //it will test it to the 'current day' of September 1 2045 UTC or 2393107200000
+    const realDateNow = Date.now.bind(global.Date);
+    const dateNowStub = jest.fn(() => 2393107200000);
+    global.Date.now = dateNowStub;
+
+    age = new GalacticAge("July 1 2020 UTC");
+    expect(age.getGalacticAge("Mars")).toEqual(47);
+
+    expect(dateNowStub).toHaveBeenCalled();
+    global.Date.now = realDateNow;
+  });
+
+
+
+});
