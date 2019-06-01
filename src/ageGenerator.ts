6599b52ea2b27add1ad42c800cc6eb0f205f048d
Todd Aden <FluentZap@Gmail.com>
2019-05-31 18:12:17 -0700

    add edge cases for getLongevity

--- src/ageGenerator.ts
+++ src/ageGenerator.ts
@@ -1,11 +1,11 @@
 import lifeData from '../src/lifeExpectancy.json';
 
-const ageModifiers = {       
-    "earth": 1,
-    "mercury": 0.24,
-    "venus": 0.62,
-    "mars": 1.88,
-    "jupiter": 11.86    
+const ageModifiers = {
+  "earth": 1,
+  "mercury": 0.24,
+  "venus": 0.62,
+  "mars": 1.88,
+  "jupiter": 11.86
 }
 
 export class GalacticAge {
@@ -25,42 +25,45 @@ export class GalacticAge {
       this.earthAge = new Date(age).getUTCFullYear() - 1970;
     }
   }
-  
-  
-  getGalacticAge(): number
+
+
   getGalacticAge(planet: string): number
-  getGalacticAge(planet?: string)
-  {
-    planet = planet.toLowerCase();
-    if (typeof planet === 'undefined' || !(planet in ageModifiers)) {
-      planet = "Earth";
-    }    
+  getGalacticAge(planet?: string) {
+    if (planet === undefined || !((planet = planet.toLowerCase()) in ageModifiers)) {
+      planet = "earth";
+    }
     return Math.floor(this.earthAge * ageModifiers[planet]);
   }
 
   getGalacticLongevity(): number
   getGalacticLongevity(planet: string): number
   getGalacticLongevity(planet?: string) {
-    planet = planet.toLowerCase();
-    if (typeof planet === 'undefined' || !(planet in ageModifiers)) {
-      planet = "Earth";
+    if (planet === undefined || !((planet = planet.toLowerCase()) in ageModifiers)) {
+      planet = "earth";
     }
-    // console.log(this.earthAge);
-    // console.log(this.birthYear.toString());
-    console.log((this.getLongevity(this.birthYear.toString())));
-    
-    return Math.floor((this.getLongevity(this.birthYear.toString()) - this.earthAge) * ageModifiers[planet]);
+
+    if (this.birthYear > new Date(Date.now()).getUTCFullYear()) {
+      return Math.floor(this.getLongevity(this.birthYear.toString()) * ageModifiers[planet]);
+    } else {
+      return Math.floor((this.getLongevity(this.birthYear.toString()) - this.earthAge) * ageModifiers[planet]);
+    }
   }
-  
-  getLongevity(year: string, gender?: string) : number {
+
+  getLongevity(year: string, gender?: string): number {
+    let yearNumber: number = parseInt(year);
+    if (yearNumber < 1900) {
+      return 47;
+    }
+    if (yearNumber > 2014) {
+      return 80;
+    }    
     for (let i = 0; i < lifeData.data.length; i++) {
       const element = lifeData.data[i];
       if (element[0] == year && element[1] === "Both Sexes") {
         return parseInt(element[2]);
-      }      
+      }
     }    
-    return 80;
   }
 
- }
+}
 
